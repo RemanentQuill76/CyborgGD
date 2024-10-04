b6c72b7a86e1aa2ff84155849fede6dde2af1c83
Burgz-MX <maxwell.burgdorf@gmail.com>
2024-10-04 15:46:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,22 @@
 public class Main {
-<<<<<<< HEAD
+  
   public static void main(String[] args) {
     Cyborg basicCyborg = new Cyborg("Basic-01", "CyborgCorp");
     Cyborg visionCyborg = new EnhancedVisionCyborg("VisionX", "OptiTech", "Night Vision");
-=======
+
     public static void main(String[] args) {
         Cyborg basicCyborg = new Cyborg("Basic-01", "CyborgCorp");
         Cyborg visionCyborg = new EnhancedVisionCyborg("VisionX", "OptiTech", "Night Vision");
         Cyborg strongCyborg = new SuperStrengthCyborg("StrengthX", "OptiTech", "Increased Durability", "Heavy Lifting");
->>>>>>> 349845b272103a47ef0ae52a04368d9b481c03b6
 
     basicCyborg.displayInfo();
     basicCyborg.performTask();
 
     System.out.println();
-
-<<<<<<< HEAD
+  
     visionCyborg.displayInfo();
     visionCyborg.performTask();
   }
-=======
         visionCyborg.displayInfo();
 
         visionCyborg.performTask();
@@ -27,5 +24,4 @@ public class Main {
         strongCyborg.displayInfo();
         strongCyborg.performTask();
     }
->>>>>>> 349845b272103a47ef0ae52a04368d9b481c03b6
 }
