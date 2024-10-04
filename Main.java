d74e870ba40bfad22790d26e76fb394b908e9b79
root <root@ogLegion76>
2024-10-04 15:38:11 -0400

    george droyd

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        Cyborg basicCyborg = new Cyborg("Basic-01", "CyborgCorp");
-        Cyborg visionCyborg = new EnhancedVisionCyborg("VisionX", "OptiTech", "Night Vision");
+  public static void main(String[] args) {
+    Cyborg basicCyborg = new Cyborg("Basic-01", "CyborgCorp");
+    Cyborg visionCyborg = new EnhancedVisionCyborg("VisionX", "OptiTech", "Night Vision");
 
-        basicCyborg.displayInfo();
-        basicCyborg.performTask();
+    basicCyborg.displayInfo();
+    basicCyborg.performTask();
 
-        System.out.println();
+    System.out.println();
 
-        visionCyborg.displayInfo();
-        visionCyborg.performTask();
-    }
+    visionCyborg.displayInfo();
+    visionCyborg.performTask();
+  }
 }
