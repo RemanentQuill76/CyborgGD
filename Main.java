171418aef4df82a8a00134c09c866d0cba224a25
root <root@ogLegion76>
2024-10-07 14:10:06 -0400

    fixed

--- Main.java
+++ Main.java
@@ -1,27 +1,18 @@
 public class Main {
-  
   public static void main(String[] args) {
     Cyborg basicCyborg = new Cyborg("Basic-01", "CyborgCorp");
     Cyborg visionCyborg = new EnhancedVisionCyborg("VisionX", "OptiTech", "Night Vision");
-
-    public static void main(String[] args) {
-        Cyborg basicCyborg = new Cyborg("Basic-01", "CyborgCorp");
-        Cyborg visionCyborg = new EnhancedVisionCyborg("VisionX", "OptiTech", "Night Vision");
-        Cyborg strongCyborg = new SuperStrengthCyborg("StrengthX", "OptiTech", "Increased Durability", "Heavy Lifting");
+    Cyborg strongCyborg = new SuperStrengthCyborg("StrengthX", "OptiTech", "Increased Durability", "Heavy Lifting");
 
     basicCyborg.displayInfo();
     basicCyborg.performTask();
 
-    System.out.println();
-  
     visionCyborg.displayInfo();
     visionCyborg.performTask();
+    visionCyborg.displayInfo();
+    visionCyborg.performTask();
+
+    strongCyborg.displayInfo();
+    strongCyborg.performTask();
   }
-        visionCyborg.displayInfo();
-
-        visionCyborg.performTask();
-
-        strongCyborg.displayInfo();
-        strongCyborg.performTask();
-    }
 }
